55daa223e8be6c16aff1229d79b8faad242e8560
haeb98 <nice2pinky@gmail.com>
2024-10-04 02:52:09 +0900

    Update: user_menu 중 '새로주문' 기능 업데이트 (추가 주문, 선택메뉴 출력, 카트 담기, 카트 출력 추가)

--- MySubway/repository/menu_repo.py
+++ MySubway/repository/menu_repo.py
@@ -9,43 +9,69 @@ class MenuRepo:
         4: MenuSelection(4, 'sandwich', '스파이시 이탈리안(Spicy ltalian)',6_500),
         5: MenuSelection(5, 'sandwich', '치킨 데리야끼(Chicken Teriyaki)', 6_500)
     }
-    # sandwich_default = ['에그마요(Egg Mayo)', '비엘티(B.L.T.)', '이탈리안 비엠티(ltalian B.M.T.)', '스파이시 이탈리안(Spicy ltalian)',
+
+    # sandwich_list = ['에그마요(Egg Mayo)', '비엘티(B.L.T.)', '이탈리안 비엠티(ltalian B.M.T.)', '스파이시 이탈리안(Spicy ltalian)',
     #                     '치킨 데리야끼(Chicken Teriyaki)', '써브웨이 클럽(Subway Club)', '폴드 포크 바비큐(Pulled Pork Barbecue)',
     #                     'K-바비큐(K-BBQ)', '로티세리 바비큐 치킨(Rotisserie Barbecue Chicken)']
-    # sandwich_dict = {i + 1: sandwich for i, sandwich in enumerate(sandwich_default)}
+    # sandwich_dict = {i + 1: sandwich for i, sandwich in enumerate(sandwich_list)}
 
-    bread_list = ['화이트(White Bread)', '파마산 오레가노(Parmesan Oregano)', '위트(Whole Wheat)', '허니 오트(Honey Oat)', '하티(Hearty)',
-                  '플랫 브레드(Flat Bread)']
+    # bread_list = ['화이트(White Bread)', '파마산 오레가노(Parmesan Oregano)', '위트(Whole Wheat)', '허니 오트(Honey Oat)', '하티(Hearty)',
+    #               '플랫 브레드(Flat Bread)']
     # bread_dict = {i + 1: bread for i, bread in enumerate(bread_list)}
+    # print(bread_dict)
     bread_dict = {
-        1: MenuOption(1, 'bread','화이트(White Bread)' ),
-        2: MenuOption(2, 'bread', '파마산 오레가노(Parmesan Oregano)')
+        1: MenuOption(1, 'bread','화이트' ),
+        2: MenuOption(2, 'bread', '파마산 오레가노'),
+        3: MenuOption(3, 'bread', '위트'),
+        4: MenuOption(4, 'bread', '허니 오트'),
+        5: MenuOption(5, 'bread', '하티'),
+        6: MenuOption(6, 'bread', '플랫 브레드')
     }
 
-    cheese_list = ['아메리칸 치즈(American Cheese)', '슈레드 치즈(Shredded Cheese)', '모차렐라 치즈(Mozzarella Cheese)', '치즈 제외']
+    # cheese_list = ['아메리칸 치즈(American Cheese)', '슈레드 치즈(Shredded Cheese)', '모차렐라 치즈(Mozzarella Cheese)', '치즈 제외']
     # cheese_dict = {i + 1: cheese for i, cheese in enumerate(cheese_list)}
     cheese_dict = {
-        1: MenuOption(1, 'cheese', '아메리칸 치즈(American Cheese)'),
+        1: MenuOption(1, 'cheese', '아메리칸 치즈'),
+        2: MenuOption(2, 'cheese', '슈레드 치즈'),
+        3: MenuOption(3, 'cheese', '모차렐라 치즈'),
+        4: MenuOption(4, 'cheese', '치즈 제외')
     }
 
-    veggies_list = ['양상추', '토마토', '오이', '피망', '양파', '피클', '올리브', '할라피뇨', '모두 넣기', '모두 빼기']
+    # veggies_list = ['양상추', '토마토', '오이', '피망', '양파', '피클', '올리브', '할라피뇨', '모두 넣기', '모두 빼기']
     # veggies_dict = {i + 1: veggies for i, veggies in enumerate(veggies_list)}
     veggies_dict = {
         1: MenuOption(1, 'veggies', '양상추'),
+        2: MenuOption(2, 'veggies', '토마토'),
+        3: MenuOption(3, 'veggies', '오이'),
+        4: MenuOption(4, 'veggies', '피망'),
+        5: MenuOption(5, 'veggies', '양파'),
+        6: MenuOption(6, 'veggies', '피클'),
+        7: MenuOption(7, 'veggies', '올리브'),
+        8: MenuOption(8, 'veggies', '할라피뇨'),
+        9: MenuOption(9, 'veggies', '모두 넣기'),
+        10: MenuOption(10, 'veggies', '모두 빼기')
     }
 
 
-    sauce_list = ['랜치(Ranch)', '스위트 어니언(Sweet Onion)', '마요네즈(Mayonnaise)', '스위트 칠리(Sweet Chill)', '스모크 바비큐(Smoked BBQ)',
-                  '핫 칠리(Hot Chill)', '허니 머스타드(Honey Mustard)', '사우스웨스트 치폴레(Southwest Chipotle)', '홀스래디쉬(Horseradish)',
-                  '머스타드(Mustard)', '올리브 오일(Olive oil)', '레드와인 식초(Red wine vinegar)', '소금(salt)', '후추(pepper)', '소스 제외']
+    # sauce_list = ['랜치(Ranch)', '스위트 어니언(Sweet Onion)', '마요네즈(Mayonnaise)', '스위트 칠리(Sweet Chill)', '스모크 바비큐(Smoked BBQ)',
+    #               '핫 칠리(Hot Chill)', '허니 머스타드(Honey Mustard)', '사우스웨스트 치폴레(Southwest Chipotle)', '홀스래디쉬(Horseradish)',
+    #               '머스타드(Mustard)', '올리브 오일(Olive oil)', '레드와인 식초(Red wine vinegar)', '소금(salt)', '후추(pepper)', '소스 제외']
     # sauce_dict = {i + 1: sauce for i, sauce in enumerate(sauce_list)}
     sauce_dict = {
-        1: MenuOption(1, 'sauce','랜치(Ranch)' )
+        1: MenuOption(1, 'sauce', '랜치'),
+        2: MenuOption(2, 'sauce', '스위트 어니언'),
+        3: MenuOption(3, 'sauce', '스위트 칠리'),
+        4: MenuOption(4, 'sauce', '사우스웨스트 치폴레'),
+        5: MenuOption(5, 'sauce', '홀스래디쉬'),
+        6: MenuOption(6, 'sauce', '올리브 오일'),
+        7: MenuOption(7, 'sauce', '레드와인 식초)'),
+        8: MenuOption(8, 'sauce', '소금'),
+        9: MenuOption(9, 'sauce', '후추'),
+        10: MenuOption(10, 'sauce', '소스 제외')
     }
 
     def __init__(self):
         self.cart = []
-
 
     def add_to_cart(self, selected_menu):
         self.cart.append(selected_menu)
--- MySubway/user_menu.py
+++ MySubway/user_menu.py
@@ -1,45 +1,89 @@
 from MySubway.entity.menu_entity import MenuEntity, MenuOption, MenuSelection
 from MySubway.repository.menu_repo import MenuRepo
 from MySubway.service.menu_service import MenuService
+from MySubway.service.order_service import OrderService
 
+# from MySubway.order_menu import OrderMenu
 
+# user_Login_id로 로그인 시 UserMenu 실행
 class UserMenu:
     def __init__(self):
         self.menu_service = MenuService()
         self.menu_repo = MenuRepo()
 
     def user_menu(self):
-        print('안녕하세요. MySubway를 방문해주셔서 감사합니다.')
+        print('🏠안녕하세요. MySubway를 방문해주셔서 감사합니다.🏠')
         user_menu_str = '''
-=====MySuby Menu=====
+=====MySubway Menu=====
 1. MyMenu로 주문
 2. 새로 주문
 0. 되돌아가기
-=====================
-Enter number: '''
+=======================
+>Enter number: '''
         choice = int(input(user_menu_str))
         match choice:
             case 1:
                 self.menu_service.my_menu()
             case 2:
-                selected_menu = self.start_menu()
-                cart = self.menu_service.add_to_cart(selected_menu)
-                # print(result[0].get_selected_menu_name())
-                self.print_cart(cart)
+                while True:
+                    selected_menu = self.start_menu()
+                    self.print_selected_menu(selected_menu)
+                    cart = self.menu_service.add_to_cart(selected_menu)
+
+                    order_more_yn = input("> 추가 주문할 것이 있나요?(y/n) ")
+                    if order_more_yn != 'y':
+                        print(">>> 결제창으로 넘어갑니다.카트를 확인해주세요")
+                        self.print_cart(cart)
+                        while True:
+                            order_now_yn = input("> 이대로 주문할까요?(y/n) ")
+                            if order_now_yn == 'y':
+                                order_service = OrderService()
+                                result = order_service.order_now(user=self.get_current_user(), cart=cart)
+                                self.print_result(result)
+                                break
+                            else:
+                                break
+
+                            # order_menu = OrderMenu(user, cart)
+                            # order_menu.display_menu()
             case 0:
                 return
             case _:
-                print('잘못 입력하셨습니다')
+                print('>>>잘못 입력하셨습니다')
                 pass
                 
+#     bread_str = '''
+# 🥖빵을 골라주세요🥖
+# 1. 화이트(White Bread
+# 2. 파마산 오레가노(Parmesan Oregano
+# 3. 위트(Whole Wheat
+# 4. 허니 오트(Honey Oat
+# 5. 하티(Hearty
+# 6. 플랫 브레드(Flat Bread)
+# Enter number: '''
+    # 메뉴는 어드민에 의해 변경 가능하므로 menu_repo의 dict에서 불러옴
+    def generate_menu_str(self, type_dict): #start_menu에서 보여줄 menu_str릏 menu_repo로 부터 불러와서 만듦
+        menu_str = ''
+        for i, menu in type_dict.items():
+            menu_str += f"{i}. {menu.get_item()}\n"
+        menu_str += '> Enter number: '
+        return menu_str
 
-    def start_menu(self):
-        print('> 주문을 시작합니다')
-        sandwich_key = int(input("num: "))#TODO MenuRepo에서 불러와서 메뉴 출력
-        bread_key = int(input("num: "))
-        cheese_key = int(input("num: "))
-        veggies_key = int(input("num: "))
-        sauce_key = int(input("num: "))
+    def start_menu(self): # 샌드위치, 빵, 치즈, 야채, 소스 순서로 입력받아 selected_menu 출력
+        sandwich_str = self.generate_menu_str(self.menu_repo.sandwich_dict)
+        bread_str = self.generate_menu_str(self.menu_repo.bread_dict)
+        cheese_str = self.generate_menu_str(self.menu_repo.cheese_dict)
+        veggies_str = self.generate_menu_str(self.menu_repo.veggies_dict)
+        sauce_str = self.generate_menu_str(self.menu_repo.sauce_dict)
+
+        print("=====Processing Order=====")
+        sandwich_key = int(input(f'🥪샌드위치를 골라주세요🥪\n{sandwich_str}'))
+        bread_key = int(input(f'🥖빵을 골라주세요🥖\n{bread_str}'))
+        cheese_key = int(input(f'🧀치즈를 골라주세요🧀\n{cheese_str}'))
+        veggies_key = int(input(f'🥗야채를 골라주세요🥗\n{veggies_str}'))
+        sauce_key = int(input(f'🧂소스를 골라주세요🧂\n{sauce_str}'))
+        print("=========================")
+        #TODO 틀린 번호 입력 시 재입력
         selected_menu = MenuEntity(self.menu_repo.sandwich_dict[sandwich_key],
                                    self.menu_repo.bread_dict[bread_key],
                                    self.menu_repo.cheese_dict[cheese_key],
@@ -47,12 +91,31 @@ Enter number: '''
                                    self.menu_repo.sauce_dict[sauce_key])
         return selected_menu
 
+    def print_selected_menu(self, selected_menu):
+        print(f' 🥪샌드위치: {selected_menu.get_sandwich()._MenuSelection__item}\n'
+              f' 🥖빵: {selected_menu.get_bread()._MenuOption__item}\n'
+              f' 🧀치즈: {selected_menu.get_cheese()._MenuOption__item}\n'
+              f' 🥗야채: {selected_menu.get_veggies()._MenuOption__item}\n'
+              f' 🧂소스: {selected_menu.get_sauce()._MenuOption__item}\n'
+              ' 를 카트에 담았습니다🛒.')
+
     def print_cart(self, cart):
-        for item in cart:
-            print( f'{item.get_sandwich()._MenuSelection__item}\n'
-                   f' 빵: {item.get_bread()._MenuOption__item}\n'
-                   f' 치즈: {item.get_cheese()._MenuOption__item}\n'
-                   f' 야채: {item.get_veggies()._MenuOption__item}\n'
-                   f' 소스: {item.get_sauce()._MenuOption__item}\n'
-                   f' 가격: {item.get_sandwich()._MenuSelection__price}'
-                   '가 카트에 추가되었습니다')
+        print("\n🛒🛒🛒🛒🛒🛒🛒Cart🛒🛒🛒🛒🛒🛒🛒")
+        for index, item in enumerate(cart, start=1):
+            print( f'{index}. 🥪{item.get_sandwich()._MenuSelection__item}'
+                   f' 💲{item.get_sandwich()._MenuSelection__price}원\n'
+                   f'(🥖{item.get_bread()._MenuOption__item}, '
+                   f'🧀{item.get_cheese()._MenuOption__item}, '
+                   f'🥗{item.get_veggies()._MenuOption__item}, '
+                   f'🧂{item.get_sauce()._MenuOption__item})')
+        print("🛒🛒🛒🛒🛒🛒🛒🛒🛒🛒🛒🛒🛒🛒🛒🛒")
+
+    def get_current_user(self):
+        pass
+
+    def print_result(self, result):
+        if result > 0:
+            print('주문해주셔서 감사합니다. 샌드위치가 만들어질 동안 잠시만 기다려주새요~')
+        else:
+            print('결제 실패')
+
