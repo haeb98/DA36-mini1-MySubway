6f1caed0103334cc3b2002101d77ef7f07e2cd8a
dkrdmlanfl <dkrdmlanfl123@naver.com>
2024-10-04 12:20:51 +0900

    Feat : 관리자 메뉴 간소화 및 유저 아이디 관리 병합

--- MySubway_soovin/service/admin_service.py
+++ MySubway_soovin/service/admin_service.py
@@ -1,20 +1,50 @@
-from repository.admin_repo import AdminRepo
+from MySubway_soovin.repository.admin_repo import AdminRepo
+from MySubway_soovin.repository.user_repo import UserRepo
+from MySubway_soovin.entity.user_entity import UserEntity
 
 class AdminService:
 
-#순수익 #총매출만 구현하자. 총매출을 날자별로 확인해서 적립할 수 있도록.
+    #순수익 #총매출만 구현하자. 총매출을 날자별로 확인해서 적립할 수 있도록.
     def __init__(self) :
         self.admin_repo = AdminRepo()
-        self.sales_data = []
-        self.total_price = []
+        self.total_price = [1000,2000,3000,4000,5000]
+        self.allsales = sum(self.total_price)
+        self.user_repo = UserRepo()
 
-        #순매출
+    def add_user(self, user_id=None, login_id=None, user_name=None, user_gender=None, user_birth=None, my_menu=None):
+
+        if user_id is None:
+            user_id = UserEntity.next_id
+        if my_menu is None:
+            my_menu = []
+        user = UserEntity(user_id, login_id, user_name, user_gender, user_birth, my_menu)
+        self.user_repo.add_user(user)
+
+    def delete_user(self, user_id):
+        delete = self.user_repo.delete_user(user_id)
+        if delete:
+            print(f'{user_id}번 회원 정보를 삭제했습니다.')
+        else:
+            print(f'{user_id}번 회원은 존재하지 않습니다.')
+
+    def get_user_info(self):
+        return self.user_repo.get_all_user_info()
+
+    def find_by_id(self, user_id):
+        return self.user_repo.find_by_user_id(user_id)
+
+    def update_user_info(self, user_id, updated_info):
+        return self.user_repo.update_user(user_id, updated_info)
+
+    #총매출
     def total_sales(self):
 
-        total_sales = self.total_price*0.3
-        
+        self.total_sales = self.allsales
+        print(f'총 매출은 {self.total_sales}원 입니다.')
 
-
+    def pure_earn(self):
+        self.pure_earn = int(self.allsales*.3)
+        print(f'총 순수입은 {self.pure_earn}원 입니다.')
         # def add_sale(self, allsales):
         #     """매출을 추가하는 메소드"""
         #
@@ -39,14 +69,14 @@ class AdminService:
         #         for i, sale in enumerate(self.sales_data, start=1):
         #             print(f"{i}. {sale}원")
 
-            # def pureearn(self): #순수익
-            #     pureearns = sum(sales_data) *.3
-            #     print(f'총 순익은 {pureearns}원 입니다.')
-            #
-            # def purespand(self):#순지출
-            #     purespands =sum(sales_data) *.15
-            #     print(f"총 지출은 {purespands}원 입니다.")
-            # # 메인 프로그램
+        # def pureearn(self): #순수익
+        #     pureearns = sum(sales_data) *.3
+        #     print(f'총 순익은 {pureearns}원 입니다.')
+        #
+        # def purespand(self):#순지출
+        #     purespands =sum(sales_data) *.15
+        #     print(f"총 지출은 {purespands}원 입니다.")
+        # # 메인 프로그램
 
         def main():
             pass
